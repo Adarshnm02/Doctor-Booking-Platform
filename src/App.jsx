c728cfafa668c93e9d736433300df5343b7b0c6f
Adarsh nm <adarshnm02@gmail.com>
2025-01-27 10:57:24 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-amber-400 text-3xl text-gray-50 hover:text-8xl'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
